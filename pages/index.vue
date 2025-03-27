284f90f6b773846be6a5e6e5bcaf839eb0eafaa9
serii burduja <seriiburduja@gmail.com>
2025-03-27 18:28:23 +0200

    feat: package node engine

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="home">
+    <h3>home</h3>
+  </div>
+</template>
+
+<style lang="scss">
+.home {
+  h1 {
+    color: red;
+  }
+}
+</style>
