bb874ba8a4f0644c4dcc782db262042bb97bf7c2
serii burduja <seriiburduja@gmail.com>
2025-03-27 18:50:19 +0200

    feat: have a bug with styles

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { useHead } from '@unhead/vue'
+
+useHead({
+  link: [
+    { rel: "stylesheet", href: "/css/all.min.css" },
+    { rel: "stylesheet", href: "/css/bootstrap.min.css" },
+    { rel: "stylesheet", href: "/css/style.css" },
+    { rel: "stylesheet", href: "/css/responsive.css" }
+  ]
+});
+</script>
 <template>
   <div>
     <h3>Auth layout</h3>
