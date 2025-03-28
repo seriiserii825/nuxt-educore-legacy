09fef14c5eb3cf6d8b91dbafe91f0bde4abf14a8
serii burduja <seriiburduja@gmail.com>
2025-03-28 21:30:46 +0200

    feat: MenuItem

---
+++ components/Ui/MenuItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+type TMenuItem = {
+  title: string;
+  link: string;
+};
+
+const props = defineProps({
+  title: String,
+  icon: String,
+  url: String,
+});
+</script>
+
+<template>
+  <li class="nav-item">
+    <nuxt-link class="nav-link" to="/admin">
+      <span class="nav-link-icon d-md-none d-lg-inline-block">
+        <i :class="icon"></i>
+      </span>
+      <span class="nav-link-title">{{ title }}</span>
+    </nuxt-link>
+  </li>
+</template>
