1d8d28d066c9c80892b08d7ff5b0edeae8598e3c
serii burduja <seriiburduja@gmail.com>
2025-03-28 15:32:00 +0200

    feat: login admin

--- layouts/admin/auth.vue
+++ layouts/admin/auth.vue
@@ -8,11 +8,15 @@ useHead({
     { rel: "stylesheet", href: "/dist/css/tabler-payments.min.css?1692870487" },
     { rel: "stylesheet", href: "/dist/css/tabler-vendors.min.css?1692870487" },
     { rel: "stylesheet", href: "/dist/css/demo.min.css?1692870487" },
+    { rel: "stylesheet", href: "/css/all.min.css" },
+    { rel: "stylesheet", href: "/css/bootstrap.min.css" },
+    { rel: "stylesheet", href: "/css/style.css" },
+    { rel: "stylesheet", href: "/css/responsive.css" }
   ]
 });
 </script>
 <template>
-  <div>
+  <div  class="d-flex justify-content-center align-items-center">
     <slot />
   </div>
 </template>
--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -2,6 +2,26 @@
 definePageMeta({
   layout: "admin-auth",
 });
+
+const router = useRouter();
+const form = ref({
+  email: "",
+  password: "",
+  role: "admin",
+});
+
+const errors = ref();
+async function submitForm() {
+  try {
+    const response = await axiosInstance.post("/admin/login", form.value);
+    localStorage.setItem("token", response.data.token);
+    localStorage.setItem("user", JSON.stringify(response.data.user));
+    router.push("/");
+  } catch (error) {
+    errors.value = error.response.data.errors;
+  }
+  console.log(form.value);
+}
 </script>
 
 <template>
@@ -10,7 +30,13 @@ definePageMeta({
       <div class="container py-4 container-tight">
         <div class="mb-4 text-center">
           <a href="." class="navbar-brand navbar-brand-autodark">
-            <img src="/static/logo.svg" width="110" height="32" alt="Tabler" class="navbar-brand-image">
+            <img
+              src="/static/logo.svg"
+              width="110"
+              height="32"
+              alt="Tabler"
+              class="navbar-brand-image"
+            />
           </a>
         </div>
         <div class="card card-md">
@@ -18,33 +44,28 @@ definePageMeta({
             <h2 class="mb-4 text-center h2">Login to your account</h2>
             <form action="./" method="get" autocomplete="off" novalidate>
               <div class="mb-3">
-                <label class="form-label">Email address</label>
-                <input type="email" class="form-control" placeholder="your@email.com" autocomplete="off">
+                <InputComponent
+                  label="Email*"
+                  placeholder="Email or Username"
+                  name="email"
+                  v-model:value="form.email"
+                  :errors="errors ? errors.email : []"
+                />
               </div>
               <div class="mb-2">
-                <label class="form-label">
-                  Password
-                  <span class="form-label-description">
-                    <a href="./forgot-password.html">I forgot password</a>
-                  </span>
-                </label>
-                <div class="input-group input-group-flat">
-                  <input type="password" class="form-control"  placeholder="Your password"  autocomplete="off">
-                  <span class="input-group-text">
-                    <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
-                    </a>
-                  </span>
-                </div>
-              </div>
-              <div class="mb-2">
-                <label class="form-check">
-                  <input type="checkbox" class="form-check-input"/>
-                  <span class="form-check-label">Remember me on this device</span>
-                </label>
+                <InputComponent
+                  label="Password*"
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  v-model:value="form.password"
+                  :errors="errors ? errors.password : []"
+                />
               </div>
               <div class="form-footer">
-                <button type="submit" class="btn btn-primary w-100">Sign in</button>
+                <button type="button" @click="submitForm"  class="btn btn-primary w-100">
+                  Sign in
+                </button>
               </div>
             </form>
           </div>
