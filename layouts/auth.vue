0b041fd6bc03c8b0c736fd48683d0c0a39ce3e60
serii burduja <seriiburduja@gmail.com>
2025-03-28 12:45:28 +0200

    feat: deault layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,22 +1,17 @@
-<script setup lang="ts">
-import { useHead } from "#imports";
+<script setup>
+import { useHead } from '@unhead/vue'
 
 useHead({
   link: [
     { rel: "stylesheet", href: "/css/all.min.css" },
     { rel: "stylesheet", href: "/css/bootstrap.min.css" },
     { rel: "stylesheet", href: "/css/style.css" },
-    { rel: "stylesheet", href: "/css/responsive.css" },
-  ],
+    { rel: "stylesheet", href: "/css/responsive.css" }
+  ]
 });
-
 </script>
-
 <template>
-  <div class="default-layout">
-    <FrontHeader />
-    <MainMenu />
-    <MenuMobile />
+  <div>
     <slot />
   </div>
 </template>
