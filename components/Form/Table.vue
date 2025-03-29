e63605b58d128dc085f768c7bce3555e9c45b9b0
serii burduja <seriiburduja@gmail.com>
2025-03-29 15:49:19 +0200

    feat: table headers

---
+++ components/Form/Table.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+const props = defineProps({
+  headers: {
+    type: Array,
+    required: true,
+  },
+  rows: {
+    type: Array,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <div class="table-responsive">
+    <table class="table table-vcenter card-table">
+      <thead v-if="headers && headers.length">
+        <tr>
+          <th v-for="(header, index) in headers">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Paweł Kuna</td>
+          <td class="text-secondary">UI Designer, Training</td>
+          <td class="text-secondary">
+            <a href="#" class="text-reset">paweluna@howstuffworks.com</a>
+          </td>
+          <td class="text-secondary">User</td>
+          <td>
+            <a href="#">Edit</a>
+          </td>
+        </tr>
+        <tr>
+          <td>Jeffie Lewzey</td>
+          <td class="text-secondary">Chemical Engineer, Support</td>
+          <td class="text-secondary">
+            <a href="#" class="text-reset">jlewzey1@seesaa.net</a>
+          </td>
+          <td class="text-secondary">Admin</td>
+          <td>
+            <a href="#">Edit</a>
+          </td>
+        </tr>
+        <tr>
+          <td>Mallory Hulme</td>
+          <td class="text-secondary">Geologist IV, Support</td>
+          <td class="text-secondary">
+            <a href="#" class="text-reset">mhulme2@domainmarket.com</a>
+          </td>
+          <td class="text-secondary">User</td>
+          <td>
+            <a href="#">Edit</a>
+          </td>
+        </tr>
+        <tr>
+          <td>Dunn Slane</td>
+          <td class="text-secondary">Research Nurse, Sales</td>
+          <td class="text-secondary">
+            <a href="#" class="text-reset">dslane3@epa.gov</a>
+          </td>
+          <td class="text-secondary">Owner</td>
+          <td>
+            <a href="#">Edit</a>
+          </td>
+        </tr>
+        <tr>
+          <td>Emmy Levet</td>
+          <td class="text-secondary">VP Product Management, Accounting</td>
+          <td class="text-secondary">
+            <a href="#" class="text-reset">elevet4@senate.gov</a>
+          </td>
+          <td class="text-secondary">Admin</td>
+          <td>
+            <a href="#">Edit</a>
+          </td>
+        </tr>
+        <tr>
+          <td>Maryjo Lebarree</td>
+          <td class="text-secondary">Civil Engineer, Product Management</td>
+          <td class="text-secondary">
+            <a href="#" class="text-reset">mlebarree5@unc.edu</a>
+          </td>
+          <td class="text-secondary">User</td>
+          <td>
+            <a href="#">Edit</a>
+          </td>
+        </tr>
+        <tr>
+          <td>Egan Poetz</td>
+          <td class="text-secondary">Research Nurse, Engineering</td>
+          <td class="text-secondary">
+            <a href="#" class="text-reset">epoetz6@free.fr</a>
+          </td>
+          <td class="text-secondary">Admin</td>
+          <td>
+            <a href="#">Edit</a>
+          </td>
+        </tr>
+        <tr>
+          <td>Kellie Skingley</td>
+          <td class="text-secondary">Teacher, Services</td>
+          <td class="text-secondary">
+            <a href="#" class="text-reset">kskingley7@columbia.edu</a>
+          </td>
+          <td class="text-secondary">Owner</td>
+          <td>
+            <a href="#">Edit</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
--- pages/admin/instructor/requests.vue
+++ pages/admin/instructor/requests.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { FormTable } from "#components";
+
 definePageMeta({
   layout: "admin",
   middleware: ["admin"],
@@ -8,11 +10,14 @@ definePageMeta({
 <template>
   <div class="page-body">
     <div class="container-xl">
-      <div  class="card">
+      <div class="card">
         <div class="card-header">
           <h4 class="card-title">Form elements</h4>
         </div>
         <div class="card-body">
+          <FormTable
+            :headers="['Id', 'Title', 'Approve Status', 'Download', 'Action']"
+          />
         </div>
       </div>
     </div>
