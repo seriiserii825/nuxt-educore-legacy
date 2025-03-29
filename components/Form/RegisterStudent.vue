e94de637be46513280519625863c8ad687fc7dc6
serii burduja <seriiburduja@gmail.com>
2025-03-29 08:20:39 +0200

    feat: register instructor and student

---
+++ components/Form/RegisterStudent.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String as PropType<"Student" | "Instructor">,
+  role: String as PropType<"student" | "instructor">,
+});
+const router = useRouter();
+const form = ref({
+  name: "",
+  email: "",
+  password: "",
+  password_confirmation: "",
+  role: props.role,
+});
+
+const errors = ref();
+
+async function submitForm() {
+  try {
+    const response = await axiosInstance.post("/register", form.value);
+    localStorage.setItem("token", response.data.token);
+    const cookie_user = useCookie("user");
+    cookie_user.value = JSON.stringify(response.data.user);
+    router.push("/student/dashboard");
+  } catch (error) {
+    errors.value = error.response.data.errors;
+  }
+}
+</script>
+
+<template>
+  <form action="#">
+    <h2>Sign Up({{ title }})<span>!</span></h2>
+    <p class="new_user">
+      Already have an account?
+      <nuxt-link href="/login">Sign In</nuxt-link>
+    </p>
+    <div class="row">
+      <div class="col-xl-12">
+        <div class="wsus__login_form_input">
+          <InputComponent
+            label="Full name"
+            placeholder="Full name"
+            name="name"
+            v-model:value="form.name"
+            :errors="errors ? errors.name : []"
+          />
+        </div>
+      </div>
+      <div class="col-xl-12">
+        <div class="wsus__login_form_input">
+          <InputComponent
+            label="Email*"
+            placeholder="Email"
+            name="email"
+            v-model:value="form.email"
+            :errors="errors ? errors.email : []"
+          />
+        </div>
+      </div>
+      <div class="col-xl-12">
+        <div class="wsus__login_form_input">
+          <InputComponent
+            label="Password*"
+            placeholder="Password"
+            name="password"
+            type="password"
+            v-model:value="form.password"
+            :errors="errors ? errors.password : []"
+          />
+        </div>
+      </div>
+      <div class="col-xl-12">
+        <div class="wsus__login_form_input">
+          <InputComponent
+            label="Confirm Password*"
+            placeholder="Confirm Password"
+            name="password_confirmation"
+            type="password"
+            v-model:value="form.password_confirmation"
+            :errors="[]"
+          />
+        </div>
+      </div>
+      <div class="col-xl-12">
+        <div class="wsus__login_form_input">
+          <button type="button" @click="submitForm" class="common_btn">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  </form>
+</template>
--- pages/register.vue
+++ pages/register.vue
@@ -2,29 +2,7 @@
 definePageMeta({
   layout: "auth",
 });
-
-const router = useRouter();
-const form = ref({
-  name: "",
-  email: "",
-  password: "",
-  password_confirmation: "",
-  role: "student",
-});
-
-const errors = ref();
-
-async function submitForm() {
-  try {
-    const response = await axiosInstance.post("/register", form.value);
-    localStorage.setItem("token", response.data.token);
-    const cookie_user = useCookie("user");
-    cookie_user.value = JSON.stringify(response.data.user);
-    router.push("/student/dashboard");
-  } catch (error) {
-    errors.value = error.response.data.errors;
-  }
-}
+const active_tab = ref(0);
 </script>
 <template>
   <section class="wsus__sign_in sign_up">
@@ -43,133 +21,44 @@ async function submitForm() {
       </div>
       <div class="m-auto col-xxl-4 col-xl-5 col-lg-6 col-md-9 wow fadeInRight">
         <div class="wsus__sign_form_area">
+          <ul class="nav nav-pills" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+              <button
+                @click="active_tab = 0"
+                :class="{ active: active_tab === 0 }"
+                class="nav-link"
+                id="pills-home-tab"
+              >
+                Student
+              </button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button
+                @click="active_tab = 1"
+                :class="{ active: active_tab === 1 }"
+                class="nav-link"
+              >
+                Instructor
+              </button>
+            </li>
+          </ul>
           <div class="tab-content" id="pills-tabContent">
             <div
-              class="tab-pane fade show active"
-              id="pills-home"
-              role="tabpanel"
-              aria-labelledby="pills-home-tab"
+              :class="`tab-pane fade ${
+                active_tab === 0 ? 'show active' : null
+              }`"
               tabindex="0"
             >
-              <form action="#">
-                <h2>Sign Up<span>!</span></h2>
-                <p class="new_user">
-                  Already have an account?
-                  <nuxt-link href="/login">Sign In</nuxt-link>
-                </p>
-                <div class="row">
-                  <div class="col-xl-12">
-                    <div class="wsus__login_form_input">
-                      <InputComponent
-                        label="Full name"
-                        placeholder="Full name"
-                        name="name"
-                        v-model:value="form.name"
-                        :errors="errors ? errors.name : []"
-                      />
-                    </div>
-                  </div>
-                  <div class="col-xl-12">
-                    <div class="wsus__login_form_input">
-                      <InputComponent
-                        label="Email*"
-                        placeholder="Email"
-                        name="email"
-                        v-model:value="form.email"
-                        :errors="errors ? errors.email : []"
-                      />
-                    </div>
-                  </div>
-                  <div class="col-xl-12">
-                    <div class="wsus__login_form_input">
-                      <InputComponent
-                        label="Password*"
-                        placeholder="Password"
-                        name="password"
-                        type="password"
-                        v-model:value="form.password"
-                        :errors="errors ? errors.password : []"
-                      />
-                    </div>
-                  </div>
-                  <div class="col-xl-12">
-                    <div class="wsus__login_form_input">
-                      <InputComponent
-                        label="Confirm Password*"
-                        placeholder="Confirm Password"
-                        name="password_confirmation"
-                        type="password"
-                        v-model:value="form.password_confirmation"
-                        :errors="[]"
-                      />
-                    </div>
-                  </div>
-                  <div class="col-xl-12">
-                    <div class="wsus__login_form_input">
-                      <button type="button" @click="submitForm" class="common_btn">Sign Up</button>
-                    </div>
-                  </div>
-                </div>
-              </form>
+              <FormRegisterStudent title="Student" role="student" />
             </div>
 
             <div
-              class="tab-pane fade"
-              id="pills-profile"
-              role="tabpanel"
-              aria-labelledby="pills-profile-tab"
+              :class="`tab-pane fade ${
+                active_tab === 1 ? 'show active' : null
+              }`"
               tabindex="0"
             >
-              <form action="#">
-                <h2>Sign Up<span>!</span></h2>
-                <p class="new_user">
-                  Already have an account? <a href="sign_in.html">Sign In</a>
-                </p>
-                <div class="row">
-                  <div class="col-xl-12">
-                    <div class="wsus__login_form_input">
-                      <label>First name</label>
-                      <input type="text" placeholder="First name" />
-                    </div>
-                  </div>
-                  <div class="col-xl-12">
-                    <div class="wsus__login_form_input">
-                      <label>Last name</label>
-                      <input type="text" placeholder="Last name" />
-                    </div>
-                  </div>
-                  <div class="col-xl-12">
-                    <div class="wsus__login_form_input">
-                      <label>Your email</label>
-                      <input type="email" placeholder="Your email" />
-                    </div>
-                  </div>
-                  <div class="col-xl-12">
-                    <div class="wsus__login_form_input">
-                      <label>Password</label>
-                      <input type="password" placeholder="Your password" />
-                    </div>
-                  </div>
-                  <div class="col-xl-12">
-                    <div class="wsus__login_form_input">
-                      <div class="form-check">
-                        <input
-                          class="form-check-input"
-                          type="checkbox"
-                          value=""
-                          id="flexCheckDefault1"
-                        />
-                        <label class="form-check-label" for="flexCheckDefault1">
-                          By clicking Create account, I agree that I have read
-                          and accepted the <a href="#">Terms of Use</a> and
-                          <a href="#">Privacy Policy.</a>
-                        </label>
-                      </div>
-                      <button type="submit" class="common_btn">Sign Up</button>
-                    </div>
-                  </div>
-                </div>
-              </form>
+              <FormRegisterStudent title="Instructor" role="instructor" />
             </div>
           </div>
         </div>
