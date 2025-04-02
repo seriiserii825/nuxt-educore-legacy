ea603978b3aad31658c89473cd5e8c81e5b1bb55
serii burduja <seriiburduja@gmail.com>
2025-04-02 20:00:32 +0300

    feat: create lang

--- pages/admin/courses/languages/index.vue
+++ pages/admin/courses/languages/index.vue
@@ -6,16 +6,23 @@ definePageMeta({
   middleware: ["admin"],
 });
 
+const loading = ref<boolean>(false);
+
 const languages = ref<TLanguage[]>([]);
 
 async function getLanguages() {
+  loading.value = true;
   try {
     const data = await axiosInstance.get("/admin/courses/languages");
     languages.value = data.data;
+    setTimeout(() => {
+      loading.value = false;
+    }, 1000);
   } catch (error: any) {
     if (error.response.data.message) {
       console.log(error.response.data.message);
       useSweetAlert("error", "Error", error.response.data.message);
+      loading.value = false;
     }
   }
 }
@@ -34,21 +41,24 @@ onMounted(() => {
         link_text="Create Language"
       >
         <FormTable :headers="['Id', 'Name', 'Slug', 'Action']">
-          <tr v-for="lang in languages" :key="lang.id">
-            <td>{{ lang.id }}</td>
-            <td>{{ lang.name }}</td>
-            <td>{{ lang.slug }}</td>
-            <td>
-              <div class="d-flex gap-2">
-                <nuxt-link
-                  to="/admin/courses/languages/edit"
-                  class="fa fa-edit text-primary"
-                >
-                </nuxt-link>
-                <span class="fa fa-trash text-danger"></span>
-              </div>
-            </td>
-          </tr>
+          <UiLoading v-if="loading" />
+          <template v-else>
+            <tr v-for="lang in languages" :key="lang.id">
+              <td>{{ lang.id }}</td>
+              <td>{{ lang.name }}</td>
+              <td>{{ lang.slug }}</td>
+              <td>
+                <div class="d-flex gap-2">
+                  <nuxt-link
+                    to="/admin/courses/languages/edit"
+                    class="fa fa-edit text-primary"
+                  >
+                  </nuxt-link>
+                  <span class="fa fa-trash text-danger"></span>
+                </div>
+              </td>
+            </tr>
+          </template>
         </FormTable>
       </UiCard>
     </div>
