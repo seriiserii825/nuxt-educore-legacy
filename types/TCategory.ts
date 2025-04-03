11d83505f9b205a950da8ee1016a50f1535f9d25
serii burduja <seriiburduja@gmail.com>
2025-04-03 11:42:48 +0300

    feat: tcategory default values

--- types/TCategory.ts
+++ types/TCategory.ts
@@ -2,9 +2,9 @@ export type TCategory = {
   id: number;
   name: string;
   slug: string;
-  image: string;
-  icon: string;
-  parent_id: number;
-  show_at_tranding: 1 | 0;
-  status: 1 | 0;
+  image?: string;
+  icon?: string;
+  parent_id?: number;
+  show_at_tranding?: 1 | 0;
+  status?: 1 | 0;
 }
