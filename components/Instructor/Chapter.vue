eaf0f546e7d0fb9d9a01bf96113b57c14133b51a
serii burduja <seriiburduja@gmail.com>
2025-04-08 09:09:58 +0300

    feat: Lesson loop

--- components/Instructor/Chapter.vue
+++ components/Instructor/Chapter.vue
@@ -1,19 +1,46 @@
 <script setup lang="ts">
 import type { TCourseChapter } from "~/types/TCourseChapter";
-const emits = defineEmits(['emit_delete', 'emit_edit'])
+import type {TLesson} from "~/types/TLesson";
+const emits = defineEmits(["emit_delete", "emit_edit"]);
 const props = defineProps({
   chapter: {
     type: Object as PropType<TCourseChapter>,
     required: true,
   },
 });
+const route = useRoute();
+const course_id = route.params.course_id;
+const lessons = ref<TLesson[]>();
+const errors = ref();
+const loading = ref(false);
 
 function deleteChapter() {
-  emits('emit_delete', props.chapter.id);
+  emits("emit_delete", props.chapter.id);
 }
 function emitEdit() {
-  emits('emit_edit', props.chapter);
+  emits("emit_edit", props.chapter);
 }
+
+async function getLessons() {
+  loading.value = true;
+  try {
+    const data = await axiosInstance.get(
+      `/instructor/course/${course_id}/lessons`
+    );
+    lessons.value = data.data;
+    setTimeout(() => {
+      loading.value = false;
+    }, 1000);
+  } catch (error) {
+    console.error("Error fetching lessons:", error);
+    handleAxiosError(error, errors);
+    loading.value = false;
+  }
+}
+
+onMounted(() => {
+  getLessons();
+});
 </script>
 
 <template>
@@ -27,7 +54,7 @@ function emitEdit() {
         aria-expanded="true"
         aria-controls="collapseOne"
       >
-      <span>{{ chapter.title }}</span>
+        <span>{{ chapter.title }}</span>
       </button>
       <div class="add_course_content_action_btn">
         <div class="dropdown">
@@ -51,8 +78,12 @@ function emitEdit() {
             </li>
           </ul>
         </div>
-        <a class="edit" @click.prevent="emitEdit" href="#"><i class="far fa-edit"></i></a>
-        <a class="del" @click.prevent="deleteChapter" href="#"><i class="fas fa-trash-alt"></i></a>
+        <a class="edit" @click.prevent="emitEdit" href="#"
+          ><i class="far fa-edit"></i
+        ></a>
+        <a class="del" @click.prevent="deleteChapter" href="#"
+          ><i class="fas fa-trash-alt"></i
+        ></a>
       </div>
     </h2>
     <div
@@ -61,9 +92,13 @@ function emitEdit() {
       data-bs-parent="#accordionExample"
     >
       <div class="accordion-body">
-        <ul class="item_list">
-          <li>
-            <span>Aut autem dolorem debitis mollitia.</span>
+        <UiLoading v-if="loading" />
+        <ul v-else class="item_list">
+          <li
+            v-for="lesson in lessons"
+            :key="lesson.id"
+            >
+            <span>{{ lesson.title }}</span>
             <div class="add_course_content_action_btn">
               <a class="edit" href="#"><i class="far fa-edit"></i></a>
               <a class="del" href="#"><i class="fas fa-trash-alt"></i></a>
---
+++ types/TLesson.ts
@@ -0,0 +1,10 @@
+export type TLesson = {
+  id: number;
+  title: string;
+  course_id: number;
+  instructor_id: number;
+  order: number;
+  status: number;
+  created_at: string;
+  updated_at: string;
+}
