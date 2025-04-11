16ca2335def7529a662e7d2e1d90a84c7700b272
serii burduja <seriiburduja@gmail.com>
2025-04-11 11:34:35 +0300

    feat: refactor styleguide

--- pages/styleguide.vue
+++ pages/styleguide.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
-import {StyleguideSwitch} from '#components';
+import {
+  StyleguideButtons,
+  StyleguideCheckbox,
+  StyleguideFileUpload,
+  StyleguideInput,
+  StyleguideRadio,
+  StyleguideSelect,
+  StyleguideSelectGroup,
+  StyleguideSwitch,
+  StyleguideTextarea,
+} from "#components";
 
 const current_active = ref(0);
-const layout = ref(null);
+const layout = ref<HTMLElement | null>(null);
+
+const components = [
+  { name: "Button", component: StyleguideButtons },
+  { name: "Input", component: StyleguideInput },
+  { name: "Textarea", component: StyleguideTextarea },
+  { name: "Checkbox", component: StyleguideCheckbox },
+  { name: "Radio", component: StyleguideRadio },
+  { name: "Switch", component: StyleguideSwitch },
+  { name: "Select", component: StyleguideSelect },
+  { name: "Select Group", component: StyleguideSelectGroup },
+  { name: "File upload", component: StyleguideFileUpload },
+];
+
 onMounted(() => {
   if (layout.value) {
     layout.value.scrollIntoView({
@@ -18,115 +41,22 @@ onMounted(() => {
     <div class="container">
       <div class="row">
         <div class="col-md-3">
-          <button
-            :class="{ active: current_active == 0 }"
-            @click="current_active = 0"
-            class="link d-block mb-2"
-          >
-            Button
-          </button>
-          <button
-            :class="{ active: current_active == 1 }"
-            @click="current_active = 1"
-            class="link d-block mb-2"
-          >
-            Checkbox
-          </button>
-          <button
-            :class="{ active: current_active == 2 }"
-            @click="current_active = 2"
-            class="link d-block mb-2"
-          >
-          Radio
-          </button>
-          <button
-            :class="{ active: current_active == 3 }"
-            @click="current_active = 3"
-            class="link d-block mb-2"
-          >
-          Input
-          </button>
-          <button
-            :class="{ active: current_active == 4 }"
-            @click="current_active = 4"
-            class="link d-block mb-2"
-          >
-          Select
-          </button>
-          <button
-            :class="{ active: current_active == 5 }"
-            @click="current_active = 5"
-            class="link d-block mb-2"
-          >
-          Select Group
-          </button>
-          <button
-            :class="{ active: current_active == 6 }"
-            @click="current_active = 6"
-            class="link d-block mb-2"
-          >
-          File upload
-          </button>
-          <button
-            :class="{ active: current_active == 7 }"
-            @click="current_active = 7"
-            class="link d-block mb-2"
-          >
-          Textarea
-          </button>
-          <button
-            :class="{ active: current_active == 8 }"
-            @click="current_active = 8"
-            class="link d-block mb-2"
-          >
-          Switch
-          </button>
+          <div class="d-flex gap-4 flex-wrap">
+            <button
+              v-for="(component, index) in components"
+              :key="index"
+              :class="{ active: current_active === index }"
+              class="btn"
+              @click="current_active = index"
+            >
+              {{ component.name }}
+            </button>
+          </div>
         </div>
         <div class="col-md-9">
-          <div v-if="current_active === 0">
-            <StyleguideButtons />
-          </div>
-          <div v-if="current_active === 1">
-            <StyleguideCheckbox />
-          </div>
-          <div v-if="current_active === 2">
-            <StyleguideRadio />
-          </div>
-          <div v-if="current_active === 3">
-            <StyleguideInput />
-          </div>
-          <div v-if="current_active === 4">
-            <StyleguideSelect />
-          </div>
-          <div v-if="current_active === 5">
-            <StyleguideSelectGroup />
-          </div>
-          <div v-if="current_active === 6">
-            <StyleguideFileUpload />
-          </div>
-          <div v-if="current_active === 7">
-            <StyleguideTextarea />
-          </div>
-          <div v-if="current_active === 8">
-            <StyleguideSwitch />
-          </div>
+          <component :is="components[current_active].component" />
         </div>
       </div>
     </div>
   </div>
 </template>
-<style>
-.link {
-  padding: 10px 26px;
-  line-height: 1.5;
-  font-size: 16px;
-  font-weight: 500;
-  transition: color 0.5s;
-  border-radius: 8px;
-  &.active,
-  &:hover {
-    color: #fff;
-    background-color: #007bff;
-  }
-}
-</style>
