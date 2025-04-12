1c90ea657a45cab57fc4a373036353afbbf9f44b
serii burduja <seriiburduja@gmail.com>
2025-04-12 20:05:06 +0300

    feat: settings body

---
+++ components/Settings/Sidebar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card-body">
+    <div class="list-group list-group-transparent">
+      <a
+        href="./settings.html"
+        class="list-group-item list-group-item-action d-flex align-items-center active"
+        >My Account</a
+      >
+      <a
+        href="#"
+        class="list-group-item list-group-item-action d-flex align-items-center"
+        >My Notifications</a
+      >
+      <a
+        href="#"
+        class="list-group-item list-group-item-action d-flex align-items-center"
+        >Connected Apps</a
+      >
+      <a
+        href="./settings-plan.html"
+        class="list-group-item list-group-item-action d-flex align-items-center"
+        >Plans</a
+      >
+      <a
+        href="#"
+        class="list-group-item list-group-item-action d-flex align-items-center"
+        >Billing & Invoices</a
+      >
+    </div>
+  </div>
+</template>
