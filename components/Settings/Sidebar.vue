deb2110e63827958b21f3c2f22bb0b9e85200e86
serii burduja <seriiburduja@gmail.com>
2025-04-12 20:39:49 +0300

    feat: sidebar link

--- components/Settings/Sidebar.vue
+++ components/Settings/Sidebar.vue
@@ -3,31 +3,7 @@
 <template>
   <div class="card-body">
     <div class="list-group list-group-transparent">
-      <a
-        href="./settings.html"
-        class="list-group-item list-group-item-action d-flex align-items-center active"
-        >My Account</a
-      >
-      <a
-        href="#"
-        class="list-group-item list-group-item-action d-flex align-items-center"
-        >My Notifications</a
-      >
-      <a
-        href="#"
-        class="list-group-item list-group-item-action d-flex align-items-center"
-        >Connected Apps</a
-      >
-      <a
-        href="./settings-plan.html"
-        class="list-group-item list-group-item-action d-flex align-items-center"
-        >Plans</a
-      >
-      <a
-        href="#"
-        class="list-group-item list-group-item-action d-flex align-items-center"
-        >Billing & Invoices</a
-      >
+      <SettingsSidebarLink url="/admin/settings" text="Settings" />
     </div>
   </div>
 </template>
---
+++ components/Settings/SidebarLink.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const route = useRoute();
+const props = defineProps({
+  url: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+const is_active = computed(() => {
+  return route.path === props.url;
+});
+</script>
+
+<template>
+  <nuxt-link
+    :to="url"
+    class="list-group-item list-group-item-action d-flex align-items-center"
+    :class="{ active: is_active }">
+    {{ text }}
+  </nuxt-link>
+</template>
